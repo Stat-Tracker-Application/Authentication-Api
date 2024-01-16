8a624269379366036d296d89558bb1cf571bc078
Hagoort, Bart <42860246+LokiTheShadySpirit@users.noreply.github.com>
2024-01-16 13:09:34 +0100

    fixed a syntax error

--- Api/API.js
+++ Api/API.js
@@ -50,10 +50,10 @@ async function sendUserQueueMessage(message) {
 }
 
 async function receiveUserQueueMessage() {
-  const connection = await amqp.connect('amqp://rabbitmq:5672');
+  const connection = await amqp.connect("amqp://rabbitmq:5672");
   const channel = await connection.createChannel();
 
-  const queue = 'user_queue';
+  const queue = "user_queue";
 
   channel.assertQueue(queue, { durable: false });
   console.log(`Waiting for messages from ${queue}`);
@@ -65,71 +65,69 @@ async function receiveUserQueueMessage() {
     }
   });
 
-app.get("/", function (req, res) {
-  res.json({
-    message: "Hello world from auth api",
-  });
-});
-
-app.post("/user/signup", async function (req, res) {
-  try {
-    const hashedpassword = await bcryptjs.hash(req.body.password, 10);
-    const username = await req.body.username;
-
-    const newuser = new AuthModel({ username, hashedpassword });
-
-    await newuser.save().then(
-      sendUserQueueMessage("New user created.")
-    )
-
-    res.status(201).json({
-      username: newuser.username,
-      hashedpassword: newuser.hashedpassword,
-      TimeOfCreation: newuser.TimeOfCreation,
+  app.get("/", function (req, res) {
+    res.json({
+      message: "Hello world from auth api",
     });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.post("/user/login", async function (req, res) {
-  const user = await AuthModel.findOne({ username: req.body.username });
-  if (user === null) {
-    return res.status(404).send("Cannot find that username");
-  }
-  try {
-    if (await bcryptjs.compare(req.body.password, user.hashedpassword)) {
-      const tokenuser = { username: user.username };
-      const accestoken = jwt.sign(tokenuser, process.env.ACCESS_TOKEN_SECRET);
-      console.log(process.env.ACCESS_TOKEN_SECRET);
-
-      res
-        .status(200)
-        .json({ message: "Succesfully logged in", accestoken: accestoken });
-    } else {
-      res.status(401).send("incorrect username or password");
-    }
-  } catch (error) {
-    res.status(500).json({
-      error: error.message,
-      accestoken: process.env.ACCESS_TOKEN_SECRET,
-    });
-  }
-});
-
-app.get("/authenticatetoken", async function (req, res) {
-  const autheader = req.headers["authorization"];
-  const token = autheader?.split(" ")[1]; // Header should be Bearer Token so the token is after the space.\
-
-  if (token === undefined || token === null) res.sendStatus(401);
-
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, user) {
-    if (err) {
-      console.log(process.env.ACCESS_TOKEN_SECRET);
-      res.sendStatus(403);
-    }
-    res.status(200).json({ user });
   });
-});
 
+  app.post("/user/signup", async function (req, res) {
+    try {
+      const hashedpassword = await bcryptjs.hash(req.body.password, 10);
+      const username = await req.body.username;
+
+      const newuser = new AuthModel({ username, hashedpassword });
+
+      await newuser.save().then(sendUserQueueMessage("New user created."));
+
+      res.status(201).json({
+        username: newuser.username,
+        hashedpassword: newuser.hashedpassword,
+        TimeOfCreation: newuser.TimeOfCreation,
+      });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+  });
+
+  app.post("/user/login", async function (req, res) {
+    const user = await AuthModel.findOne({ username: req.body.username });
+    if (user === null) {
+      return res.status(404).send("Cannot find that username");
+    }
+    try {
+      if (await bcryptjs.compare(req.body.password, user.hashedpassword)) {
+        const tokenuser = { username: user.username };
+        const accestoken = jwt.sign(tokenuser, process.env.ACCESS_TOKEN_SECRET);
+        console.log(process.env.ACCESS_TOKEN_SECRET);
+
+        res
+          .status(200)
+          .json({ message: "Succesfully logged in", accestoken: accestoken });
+      } else {
+        res.status(401).send("incorrect username or password");
+      }
+    } catch (error) {
+      res.status(500).json({
+        error: error.message,
+        accestoken: process.env.ACCESS_TOKEN_SECRET,
+      });
+    }
+  });
+
+  app.get("/authenticatetoken", async function (req, res) {
+    const autheader = req.headers["authorization"];
+    const token = autheader?.split(" ")[1]; // Header should be Bearer Token so the token is after the space.\
+
+    if (token === undefined || token === null) res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, user) {
+      if (err) {
+        console.log(process.env.ACCESS_TOKEN_SECRET);
+        res.sendStatus(403);
+      }
+      res.status(200).json({ user });
+    });
+  });
+}
 export default app;
